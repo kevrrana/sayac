22fe7e5a5b324aa52526063c6df0a854e136f5c8
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-10 14:54:48 +0300

    Add files via upload

---
+++ sayac/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace sayac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAzalt = new System.Windows.Forms.Button();
+            this.btnArtir = new System.Windows.Forms.Button();
+            this.lblSayac = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAzalt
+            // 
+            this.btnAzalt.BackColor = System.Drawing.Color.Red;
+            this.btnAzalt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAzalt.Location = new System.Drawing.Point(12, 97);
+            this.btnAzalt.Name = "btnAzalt";
+            this.btnAzalt.Size = new System.Drawing.Size(201, 126);
+            this.btnAzalt.TabIndex = 0;
+            this.btnAzalt.Text = "-";
+            this.btnAzalt.UseVisualStyleBackColor = false;
+            this.btnAzalt.Click += new System.EventHandler(this.btnAzalt_Click);
+            // 
+            // btnArtir
+            // 
+            this.btnArtir.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnArtir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnArtir.Location = new System.Drawing.Point(375, 97);
+            this.btnArtir.Name = "btnArtir";
+            this.btnArtir.Size = new System.Drawing.Size(201, 126);
+            this.btnArtir.TabIndex = 1;
+            this.btnArtir.Text = "+";
+            this.btnArtir.UseVisualStyleBackColor = false;
+            this.btnArtir.Click += new System.EventHandler(this.btnArtir_Click);
+            // 
+            // lblSayac
+            // 
+            this.lblSayac.AutoSize = true;
+            this.lblSayac.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayac.Location = new System.Drawing.Point(281, 150);
+            this.lblSayac.Name = "lblSayac";
+            this.lblSayac.Size = new System.Drawing.Size(92, 31);
+            this.lblSayac.TabIndex = 2;
+            this.lblSayac.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSayac);
+            this.Controls.Add(this.btnArtir);
+            this.Controls.Add(this.btnAzalt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAzalt;
+        private System.Windows.Forms.Button btnArtir;
+        private System.Windows.Forms.Label lblSayac;
+    }
+}
+
---
+++ sayac/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSayac.Text = "0";
+        }
+        int sayac;
+        private void btnAzalt_Click(object sender, EventArgs e)
+        {
+            if (sayac > -10)
+            {
+                sayac--;//değeri 1 azalır
+            }
+            lblSayac.Text=sayac.ToString();
+        }
+
+        private void btnArtir_Click(object sender, EventArgs e)
+        {
+            if (sayac < 10)
+            {
+                sayac++;//değeri 1 artırır
+            }
+            lblSayac.Text = sayac.ToString();
+        }
+    }
+}
